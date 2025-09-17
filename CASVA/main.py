885978f9d87ddb4ecc418c052267b9364d00c154
G-1202 <xirangao1202@163.com>
2025-09-17 21:12:27 +0800

    Update main.py

--- CASVA/main.py
+++ CASVA/main.py
@@ -8,10 +8,11 @@ if __name__ == '__main__':
     output_dir = f'Results'
     os.makedirs(output_dir, exist_ok=True)
 
-    F1, lag, bw_use, reward, bw_name, lag_1, lag_2, lag_3, lag_4, lag_5 = test(MODEL, 0, 600, 600, 0)
+    F1, lag, bw_use, reward, bw_name, lag_1, lag_2, lag_3, lag_4, lag_5 = test(MODEL)
     with open(f'test.txt', 'w') as f:
         for i in range(len(F1)):
             f.write(f"{F1[i]} {lag[i]} {bw_use[i]} {lag_1[i]} {lag_2[i]} {lag_3[i]} {lag_4[i]} {lag_5[i]}\n")
+
 
 
 
