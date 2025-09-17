c5c90dc0da684cc6598047b571c20beefa481622
G-1202 <xirangao1202@163.com>
2025-09-17 20:49:26 +0800

    Update main.py

--- CASVA/main.py
+++ CASVA/main.py
@@ -11,9 +11,10 @@ if __name__ == '__main__':
     os.makedirs(output_dir, exist_ok=True)
 
     F1, lag, bw_use, reward, bw_name, lag_1, lag_2, lag_3, lag_4, lag_5 = test(MODEL, 0, 600, TOTAL, 0)
-    with open(f'{output_dir}/{k + 1:02d}_{bw_name}', 'w') as f:
+    with open(f'test.txt', 'w') as f:
         for i in range(len(F1)):
             f.write(f"{F1[i]} {lag[i]} {bw_use[i]} {lag_1[i]} {lag_2[i]} {lag_3[i]} {lag_4[i]} {lag_5[i]}\n")
+
 
 
 
